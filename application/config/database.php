97ca4ea1360767dbe6a18535012df973cd860c13
Paul Michels <paul_michels@hotmail.com>
2019-11-19 15:42:14 +0100

    driver to postgre

--- application/config/database.php
+++ application/config/database.php
@@ -79,7 +79,7 @@ $db['default'] = array(
 	'username' => 'root',
 	'password' => '',
 	'database' => 'megamania',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
